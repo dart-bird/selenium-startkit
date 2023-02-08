8742a126c8d5313babd2bcf566ff219a4df34483
dart-bird <jwyok@hanyang.ac.kr>
2023-02-09 02:14:17 +0900

    Support darwin system

--- chrome_tools.py
+++ chrome_tools.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import subprocess
 import sys
 
@@ -15,21 +16,26 @@ def is_port_in_use(port=8989) -> bool:
 
 def open_chrome_profile(port=8989):
     if is_port_in_use(port) == False:
-        subprocess.Popen(
-        [
-            "start",
-            "chrome",
-            "--remote-debugging-port={}".format(port),
-            "--user-data-dir=" + main_directory + "/chrome_profile",
-        ],
-        shell=True,
-    )
+        if platform.system() == "Windows":
+            subprocess.Popen(
+            [
+                "start",
+                "chrome",
+                "--remote-debugging-port={}".format(port),
+                "--user-data-dir=" + main_directory + "/chrome_profile",
+            ],
+            shell=True,
+            )
 
 def get_webdriver(headless=False, port=8989) -> webdriver.Chrome:
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     opt = Options()
     if headless == True: opt.add_argument("--headless")
     opt.add_argument(f'user-agent={user_agent}')
-    opt.add_experimental_option("debuggerAddress", "localhost:{}".format(port))
+    if platform.system() == "Windows":
+        opt.add_experimental_option("debuggerAddress", "localhost:{}".format(port))
+    else:
+        opt.add_argument('user-data-dir={}/chrome_profile'.format(main_directory))
+        
     driver = webdriver.Chrome(options=opt)
     return driver
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-import chrome_tools
 import time
+import platform
 
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 
+import chrome_tools
+
 if __name__ == "__main__":
+
     chromedriver_autoinstaller.install()
     chrome_tools.open_chrome_profile()
     while True:
+        if platform.system() == "Darwin":
+            break
         if chrome_tools.is_port_in_use() == True:
             break
-    browser = chrome_tools.get_webdriver()
 
+    browser = chrome_tools.get_webdriver()
     browser.get('https://www.google.com')
 
     try:
